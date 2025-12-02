f2cb6bb4ecd02c828dbf5847a24bcd69ec312471
LaChancee <miharisoababef@gmail.com>
2025-12-02 17:37:04 +0100

    Remove AuthButton component from the Header, streamlining the layout for improved user experience in the Click&Collect system for "Les délices d'Erwann".

--- src/features/layout/header.tsx
+++ src/features/layout/header.tsx
@@ -1,10 +1,8 @@
-import { AuthButton } from "../auth/auth-button";
 import { HeaderBase } from "./header-base";
 
 export function Header() {
   return (
     <HeaderBase>
-      <AuthButton />
     </HeaderBase>
   );
 }
